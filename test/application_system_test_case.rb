8ceedab20df40a0c999fcf5276aadd598a81352f
santiago lopez saubidet <slopez95@hotmail.com>
2023-05-29 19:56:14 -0300

    Stupid Coaching Rails

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,8 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  # driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  driven_by :headless_chrome # Update this line
+  # la L5 se agrego/actualizo por parte 2 del ejs.
+  # la L4 era lo que estaba antes.
 end
